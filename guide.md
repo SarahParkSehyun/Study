5602608bfab3e3a7fcd53f01f5e672231eff466c
Jeonhui <dlwjsgml02@naver.com>
2022-06-29 19:09:48 +0900

    Study Settings

---
+++ guide.md
@@ -0,0 +1,17 @@
+## 알고리즘 스터디
+
+---
+### [신찬수 교수님 강의](https://www.youtube.com/c/ChanSuShin) 듣기
+- 모르는 건 물어보기 ㅎㅅㅎ
+
+### 매주 problem.md 파일에서 문제 확인
+- 파일명: 문제번호.py
+- test파일 실행 (test case 확인)  
+  - problem: 문제번호 
+  - name: jeonhui / sehyun / bomi
+  - 결과 출력
+  - github에 커밋하기
+  
+---
+
+- test파일 코드는 건드리지 않기! (코드는 봐도 됨)
---
+++ test.py
@@ -0,0 +1,97 @@
+import sys
+import json
+import os
+import subprocess
+
+# testcase 저장 파일 경로
+testcaseFilePath = "./testcase/testcase.json"
+
+def run_test(file_path, i, sample_input, sample_output):
+    try:
+        # 파일 경로의 파이썬 파일을 subprocess로 open
+        file = subprocess.Popen(
+            ['python3', file_path],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+    except Exception as err:
+        print("파일이 존재하지 않습니다.", err)
+        return -1
+
+    # subprocess 실행된 파일에 sample_input 전달
+    file.communicate(input=bytes(str(sample_input).rstrip(), encoding="utf-8"))
+
+    # subprocess 실행된 파일의 출력을 가져옴
+    result, err = file.communicate()
+
+    print(f"테스트케이스({i})")
+    # 에러가 발생했을 경우 -1 반환
+    err = err.decode('utf-8')
+    if err == '':
+        result = result.decode('utf-8').rstrip()
+    else:
+        print("Error\n", err)
+        return -1
+
+
+    print(f"입력 예시: {sample_input}")
+    print(f"출력 예시: {sample_output}\n출력 결과: {result}")
+    if str(sample_output).rstrip() == result:
+        print("맞았습니다!!")
+        print("-----------")
+        return 1
+    else:
+        print("틀렸습니다.")
+        print("-----------")
+        return 0
+
+
+def getTestCase(problem_number):  # json 파일에 해당 문제 testcase들을 가져와 반환
+    try:
+        with open(testcaseFilePath, "r") as json_file:
+            problem_testcase = json.load(json_file)[problem_number]
+    except Exception as err:
+        print(f"{problem_number}은 테스트 케이스에 존재하지 않는 문제번호입니다.", err)
+        return -1
+    return problem_testcase
+
+
+def __main__():
+    # 문제 번호 및 이름 입력
+    print("problem: ", end="")
+    problem_number = sys.stdin.readline().rstrip()
+    print("name: ", end="")
+    name = sys.stdin.readline().rstrip()
+    name = name.lower()
+    if name == 'j' or name == 'jeonhui':
+        name = 'Jeonhui'
+    elif name == 's' or name == 'sehyun':
+        name = 'Sehyun'
+    elif name == 'b' or name == 'bomi':
+        name = 'Bomi'
+    else:
+        print("등록되지 않은 이용자입니다.")
+        return
+
+
+    # 파일 경로 설정
+    file_path = f"{os.getcwd()}/{name}/problem/{problem_number}.py"
+
+    # 테스트 케이스 가져오기
+    testcases = getTestCase(problem_number)
+    if testcases == -1:
+        return
+
+
+    # 테스트 케이스를 성공적으로 가져왔을 경우
+    print("-----------")
+    print(f"문제 번호: {problem_number}")
+    print("-----------")
+    i = 1
+    for tc in testcases:
+        if run_test(file_path, i, tc['input'], tc['output']) == -1:
+            return
+        i += 1
+
+__main__()
