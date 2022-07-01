5230972f1641634f94ed63a9230fbab981481af0
Jeonhui <48307153+Jeonhui@users.noreply.github.com>
2022-07-01 17:20:45 +0900

    4673

--- Jeonhui/problem/4673.py
+++ Jeonhui/problem/4673.py
@@ -1,8 +1,14 @@
-NUM = [0 for _ in range(11)]
+NUM = [0 for _ in range(10001)]
 
 
 def createNUM(n):
     sumN = int(n)
     for i in range(len(n)):
         sumN += int(n[i])
-    NUM[]
+    if sumN < 10001:
+        NUM[sumN] += 1
+
+
+for i in range(10001):
+    createNUM(str(i))
+print("\n".join([str(i) for i in range(10001) if NUM[i] == 0]))
